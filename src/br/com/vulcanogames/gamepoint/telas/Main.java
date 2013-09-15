f84beb65e969f13e3f4fcc385358696cff733b0b
Demetrius Feijóo Campos <demetrius.feijoo.91@gmail.com>
2013-09-14 21:47:10 -0300

    Adicionado SherlockActionBar

--- src/br/com/vulcanogames/gamepoint/telas/Main.java
+++ src/br/com/vulcanogames/gamepoint/telas/Main.java
@@ -1,25 +1,41 @@
 package br.com.vulcanogames.gamepoint.telas;
 
-import android.app.ListActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import br.com.vulcanogames.gamepoint.MyServiceSettings;
+import br.com.vulcanogames.gamepoint.R;
+import com.actionbarsherlock.app.SherlockListActivity;
+import com.actionbarsherlock.view.Window;
 import com.asccode.tinyapi.RequestCallback;
 import com.asccode.tinyapi.Response;
 import com.asccode.tinyapi.Service;
 import com.asccode.tinyapi.model.Article;
 import com.asccode.tinyapi.model.Login;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 
 import java.util.List;
 
-public class Main extends ListActivity {
+public class Main extends SherlockListActivity{
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
 
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);  //Habilita Loading
+         /*
+        SlidingMenu menu = new SlidingMenu(this);
+        menu.setMode(SlidingMenu.LEFT);
+        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        menu.setShadowWidthRes(R.dimen.shadow_width);
+        menu.setShadowDrawable(R.drawable.shadow);
+        menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+        menu.setFadeDegree(0.35f);
+        //menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+        //menu.setMenu(R.layout.main);
+        getSherlock().getActionBar().setDisplayHomeAsUpEnabled(true);
+            */
         MyServiceSettings myServiceSettings = new MyServiceSettings(getSharedPreferences("PREFS", 0));
 
         final Service service = new Service(myServiceSettings, this);
@@ -29,17 +45,22 @@ public class Main extends ListActivity {
             service.authenticate( new RequestCallback<Login>() {
                 @Override
                 public void onStart() {
+                    setSupportProgressBarIndeterminateVisibility(true);
                 }
 
                 @Override
                 public void onSuccess(Response<Login> successResponse) {
 
+                    setSupportProgressBarIndeterminateVisibility(false);
                     service.articles(articleRequestCallback);
 
                 }
 
                 @Override
                 public void onError(Response<com.asccode.tinyapi.Error> errorResponse) {
+
+                    setSupportProgressBarIndeterminateVisibility(false);
+
                 }
             });
         }else{ //Pega os artigos diretos, pois está logado.
@@ -52,6 +73,9 @@ public class Main extends ListActivity {
     private RequestCallback articleRequestCallback = new RequestCallback<List<Article>>() {
         @Override
         public void onStart() {
+
+            setSupportProgressBarIndeterminateVisibility(true);
+
         }
 
         @Override
@@ -61,10 +85,15 @@ public class Main extends ListActivity {
 
             setListAdapter(listAdapter);
 
+            setSupportProgressBarIndeterminateVisibility(false);
+
         }
 
         @Override
         public void onError(Response<com.asccode.tinyapi.Error> errorResponse) {
+
+            setSupportProgressBarIndeterminateVisibility(false);
+
         }
     };
 
